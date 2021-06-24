5ff7d9e5d0e1607038386521e889fa0af77293a6
allenn159 <allenn159@gmail.com>
2021-06-23 21:27:47 -0700

    Finished!

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const billAmt = document.getElementById("billamt");
 const tipAmt = document.getElementById("tipamt");
 const peopleAmt = document.getElementById("peopleamt");
 const calculate = document.getElementById("calculate");
+const totalTip = document.getElementById("totaltip");
 
 const allInputs = [billAmt, tipAmt, peopleAmt];
 
@@ -10,16 +11,33 @@ allInputs.forEach((e) => {
     if (this.value > 0) {
       this.classList.add("form-control-success");
       this.classList.remove("form-control-error");
-      return true;
     } else {
       this.classList.add("form-control-error");
       this.classList.remove("form-control-success");
-      return false;
     }
   });
 });
 
+const check = () => {
+  allInputs.forEach((e) => {
+    if (e.value > 0) {
+      e.classList.add("form-control-success");
+      e.classList.remove("form-control-error");
+      calculateTotal();
+    } else {
+      e.classList.add("form-control-error");
+      e.classList.remove("form-control-success");
+      totalTip.innerHTML = "Please enter values";
+    }
+  });
+};
+
+const calculateTotal = () => {
+  const tip = ((billAmt.value * tipAmt.value) / peopleAmt.value).toFixed(2);
+  totalTip.innerHTML = `Tip per person: $${tip}`;
+};
+
 calculate.addEventListener("click", (e) => {
   e.preventDefault();
-  checkBillAmt(billAmt);
+  check();
 });
